16d03685b2d1414cd2f1de3c66775b097b848e00
Mary Lee <marylee@app.local>
2019-08-12 16:38:48 -0400

    Tables Practice Iteration 2

--- CactiTableViewController.swift
+++ CactiTableViewController.swift
@@ -8,25 +8,35 @@
 
 import UIKit
 
+struct Cacti{
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+}
 class CactiTableViewController: UITableViewController {
-
+    var allCacti = [
+    Cacti(id: 1, title: "Prickly Pear", text: "Opuntia commonly called prickly pear, is a genus in the cactus family, Cactaceae.", image: "prickly pear"),
+    Cacti(id: 2, title: "Hedgehog Cacti", text: "Echinopsis is a large genus of cacti native to South America, sometimes known as hedgehog cactus, sea-urchin cactus or Easter lily cactus.", image: "hedgehog"),
+    Cacti(id: 3, title: "Rebutia", text: "Rebutia is a genus in the family Cactaceae, native to Bolivia and Argentina. They are generally small, colorful cacti, and globular in form.", image: "rebutia")
+    ]
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 3
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return allCacti.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
 
-    cell.textLabel?.text = "Section\(indexPath.section)Row\(indexPath.row)"
+    cell.textLabel?.text = allCacti[indexPath.row].title
 
         return cell
     }
